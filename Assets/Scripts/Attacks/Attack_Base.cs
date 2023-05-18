cdabfda1cb9f093a1f48d1a2d38042f0eb8dc39c
JackOutTheBox124 <jack@jackoutthebox.dev>
2023-05-18 10:38:52 -0400

    Added attacking basic enemies

---
+++ Assets/Scripts/Attacks/Attack_Base.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Base : MonoBehaviour
+{
+    public Rigidbody2D attackRigidBody;
+    private float _lifeInSeconds = 2.5f;
+    private int _speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackRigidBody = GetComponent<Rigidbody2D>();
+        
+        if (gameObject.name.Contains("(Clone)"))
+        {
+            Destroy(gameObject, _lifeInSeconds);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackRigidBody.velocity = Vector2.up * _speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("TriggerEnter");
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Squirrel/Squirrel_Attack.cs
+++ Assets/Scripts/Squirrel/Squirrel_Attack.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class Squirrel_Attack : MonoBehaviour
 {
-    private int attackSpeed;
+    public GameObject moltenAcorn;
+    private float _attackCooldown = .15f;
+    private float lastAttack = 0;
+
+    // private int attackSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moltenAcorn = GameObject.FindWithTag("MoltenAcorn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(lastAttack + _attackCooldown < Time.time)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                lastAttack = Time.time;
+
+                Vector2 myPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+
+                Quaternion rotation = Quaternion.Euler(0, 0, 0);
+
+                Instantiate(moltenAcorn, myPos, rotation);
+            }
+
+        }
     }
+
+
 }

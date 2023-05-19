7f89a709aa7c2c80f7cb3056f9f95f9ca754eb69
Ben Lilly <benlilly2006@gmail.com>
2023-05-19 08:20:04 -0400

    It failed

--- Assets/Scripts/Enemy/Enemy_Script.cs
+++ Assets/Scripts/Enemy/Enemy_Script.cs
@@ -13,11 +13,22 @@ public class Enemy_Script : MonoBehaviour
 
     void Update()
     {
+        //Changes the direction of the enemy when it reaches a certain x world point
         if (transform.position.x < -9 || transform.position.x > 9)
         {
             speed *= -1;
         }
+        //Makes the enemy go to the left or right, and down
         gameObject.transform.position += Vector3.left * (speed/10);
         gameObject.transform.position += Vector3.down * (downSpeed / 100);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Kills the enemy when past the box collider
+        if (other.gameObject.CompareTag("PastKiller"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }

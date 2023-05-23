50ebf453115e68f2ad6b3746454073ccaa62492a
Ben Lilly <benlilly2006@gmail.com>
2023-05-22 21:10:57 -0400

    Enemy 2 looks at cat

--- Assets/Scripts/Enemy/Enemy_Script.cs
+++ Assets/Scripts/Enemy/Enemy_Script.cs
@@ -14,13 +14,7 @@ public class Enemy_Script : MonoBehaviour
 
     void Update()
     {
-        //Changes the direction of the enemy when it reaches a certain x world point
-        if (transform.position.x < -9 || transform.position.x > 9)
-        {
-            speed *= -1;
-        }
-        //Makes the enemy go to the left or right, and down
-        gameObject.transform.position += Vector3.left * (speed/10);
+        sideToSide();
         down();
     }
 
@@ -31,6 +25,17 @@ public class Enemy_Script : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+
+    public void sideToSide()
+    {
+        //Changes the direction of the enemy when it reaches a certain x world point
+        if (transform.position.x < -9 || transform.position.x > 9)
+        {
+            speed *= -1;
+        }
+        //Makes the enemy go to the left or right, and down
+        gameObject.transform.position += Vector3.left * (speed / 10);
     }
 
     public void down()
--- Assets/Scripts/Enemy/Enemy_Script2.cs
+++ Assets/Scripts/Enemy/Enemy_Script2.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Enemy_Script2 : Enemy_Script
 {
+    public Transform target;
     void Start()
     {
         
     }
     void Update()
     {
+        charge();
         down();
+    }
+
+    public void charge()
+    {
+        Vector2 direction = target.position - transform.position;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Kills the enemy when past the box collider
+        if (other.gameObject.CompareTag("PastKiller"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }

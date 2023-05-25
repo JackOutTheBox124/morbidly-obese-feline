8221cce2f11fa56e33c8e14bf34ed3afbbc99d8d
ToasterCat0622 <92898753+ToasterCat0622@users.noreply.github.com>
2023-05-25 08:30:43 -0400

    Border
    -Added Border

--- Assets/Scripts/Feline/Feline_Script.cs
+++ Assets/Scripts/Feline/Feline_Script.cs
@@ -28,6 +28,19 @@ public class Feline_Script : MonoBehaviour
         Vector3 Movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         
         felineRigidBody.transform.position += Movement * speed * Time.deltaTime;
-    
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(10.9f, transform.position.y);
+        }
+
+        if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(-10.9f, transform.position.y);
+        }
+
+        if (transform.position.y < -4.45)
+        {
+            transform.position = new Vector3(transform.position.x, -4.4f);
+        }
     }
 }
